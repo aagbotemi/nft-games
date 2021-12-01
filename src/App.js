447d07e34435c68311de090a8f2f8ba4d1a3c2a4
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-12-01 17:13:00 +0100

    connect wallet function

--- src/App.js
+++ src/App.js
@@ -36,6 +36,27 @@ function App() {
     }
   };
 
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
 
   return (
     <div className="App">
