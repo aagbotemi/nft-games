94c492c8c878d3047f4c81071ceac9b302510645
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-12-01 18:32:16 +0100

    set the gameContract into a state

--- src/components/Arena/index.js
+++ src/components/Arena/index.js
@@ -5,32 +5,51 @@ import './Arena.css';
 import LoadingIndicator from '../LoadingIndicator';
 
 const Arena = ({ characterNFT, setCharacterNFT, myEpicGame }) => {
-  const [gameContract, setGameContract] = useState(null);
-  const [boss, setBoss] = useState(null);
-  const [attackState, setAttackState] = useState('');
-  const [showToast, setShowToast] = useState(false);
+    const [gameContract, setGameContract] = useState(null);
+    const [boss, setBoss] = useState(null);
+    const [attackState, setAttackState] = useState('');
+    const [showToast, setShowToast] = useState(false);
 
-  // Actions
-  const runAttackAction = async () => {
-    try {
-      if (gameContract) {
-        setAttackState('attacking');
-        console.log('Attacking boss...');
-        const attackTxn = await gameContract.attackBoss();
-        await attackTxn.wait();
-        console.log('attackTxn:', attackTxn);
-        setAttackState('hit');
+    // Actions
+    const runAttackAction = async () => {
+        try {
+        if (gameContract) {
+            setAttackState('attacking');
+            console.log('Attacking boss...');
+            const attackTxn = await gameContract.attackBoss();
+            await attackTxn.wait();
+            console.log('attackTxn:', attackTxn);
+            setAttackState('hit');
 
-        setShowToast(true);
-        setTimeout(() => {
-          setShowToast(false);
-        }, 5000);
-      }
-    } catch (error) {
-      console.error('Error attacking boss:', error);
-      setAttackState('');
-    }
-  };
+            setShowToast(true);
+            setTimeout(() => {
+            setShowToast(false);
+            }, 5000);
+        }
+        } catch (error) {
+        console.error('Error attacking boss:', error);
+        setAttackState('');
+        }
+    };
+
+    // UseEffects
+    useEffect(() => {
+        const { ethereum } = window;
+
+        if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const gameContract = new ethers.Contract(
+            CONTRACT_ADDRESS,
+            myEpicGame.abi,
+            signer
+        );
+
+        setGameContract(gameContract);
+        } else {
+        console.log('Ethereum object not found');
+        }
+    }, []);
 
   return (
     <div className="arena-container">
