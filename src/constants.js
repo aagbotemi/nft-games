44de9e3116cab8dab87e8f71c9fe90daeba3897c
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2021-12-01 18:47:39 +0100

    transformCharacterData set in constants file

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+const CONTRACT_ADDRESS = '0x19fCB12cA2fe9d405aD97d4708306BE12e15Adb5';
+
+/*
+ * Add this method and make sure to export it on the bottom!
+ */
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+
+export { CONTRACT_ADDRESS, transformCharacterData };
